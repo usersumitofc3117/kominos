5db4b97308419fb4c7a090c433ebcb22033c6008
Sumit Kumar <your-email@example.com>
2025-08-13 09:28:33 +0530

    adding some lines not to just checking git,dont panic users

--- src/main/java/in/sp/pizzamannia/PizzaDeliveryAppApplication.java
+++ src/main/java/in/sp/pizzamannia/PizzaDeliveryAppApplication.java
@@ -9,6 +9,7 @@ public class PizzaDeliveryAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PizzaDeliveryAppApplication.class, args);
 		System.out.println("welcome gentleman the journey begins for u from now !");
+		System.out.println("all the build up in code is for u my happiness");
 	}
 
 }
